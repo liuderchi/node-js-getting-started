4b1ae9e198ad835fcc5174c56943bb2c97af43dd
liuderchi <liuderchi@gmail.com>
2021-01-26 14:15:01 +0800

    chore: add admin auth
    - node-red admin hash-pw

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -113,14 +113,17 @@ module.exports = {
   // -----------------
   // To password protect the Node-RED editor and admin API, the following
   // property can be used. See http://nodered.org/docs/security.html for details.
-  //adminAuth: {
-  //    type: "credentials",
-  //    users: [{
-  //        username: "admin",
-  //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-  //        permissions: "*"
-  //    }]
-  //},
+  adminAuth: {
+    type: 'credentials',
+    users: [
+      {
+        username: 'derek',
+        password:
+          '$2b$08$fN/nZ4JXv/bPtNbvIpzEPe9YLM1RELwC/Imps.1kf3AUDkxiys60i',
+        permissions: '*',
+      },
+    ],
+  },
 
   // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
   // the static content (httpStatic), the following properties can be used.
