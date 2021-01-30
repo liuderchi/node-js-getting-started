a8418dd16af5a757119971de1fb81feb099fe87c
liuderchi <liuderchi@gmail.com>
2021-01-31 02:05:50 +0800

    chore: introduce node-red-desktop; move .env into user directory

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * This is the default settings file provided by Node-RED.
